2732928b74e0bab86f8fe7cc7ba5a304cb79b637
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-11-13 20:00:21 +0100

    fix: Handle error loading and html tag rendering

--- src/pages/postpage.js
+++ src/pages/postpage.js
@@ -21,10 +21,9 @@ export default class PostView extends React.Component {
         return (
             <div className='posts'>
               {posts.map(post => <div key={post.id}>
-                {/* <img src={process.env.PUBLIC_URL + '/user-photo.jpg'} alt="tutor"/> */}
-                <Link to={`/${post.id}`}>{post.title.rendered}</Link>
+                <img alt="post" src={post.featured_image_thumbnail}/>
+                <Link to={`/${post.id}`}><p>{post.title.rendered}</p></Link>
                 <p>{post.slug}</p>
-                {/* <p>{post.address.city}</p> */}
               </div>)}
             </div>
         );
--- src/pages/singlepage.js
+++ src/pages/singlepage.js
@@ -4,24 +4,30 @@ export default class SinglePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            details: {}
+            details: {},
+            isLoading: true
         }
     }
 
-    componentDidMount() {    
+    componentDidMount() {
+
         const { match: { params } } = this.props;
         fetch(`https://epower.ng/wp-json/wp/v2/posts/${params.id}`)
           .then(response => response.json())
-          .then(data => this.setState({ details: data }));
+          .then(data => this.setState({ details: data, isLoading: false }));
       }
     
       render() {
-        const { details } = this.state;
+        const { details, isLoading } = this.state;
+        if (isLoading) {
+            return <p>loading...</p>;
+          }
 
         return (
             <div className='singlepage'>
-                <p>{details.slug}</p>
-                <p>{details.content.rendered}</p>
+                <p>{details.title.rendered}</p>
+                <img alt="post" src={details.featured_image}/>
+                <div dangerouslySetInnerHTML={{ __html: details.content.rendered }} />
             </div>
         );
       }
