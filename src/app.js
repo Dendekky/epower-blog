30a512c791cb57e37b8e7fe0f684f0c18219a155
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-11-13 08:22:41 +0100

    style: configure SCSS for styling

--- src/app.js
+++ src/app.js
@@ -2,17 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='div-test'>
-      <header className='App-header'>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello Epower</p>
     </div>
   )
 }
