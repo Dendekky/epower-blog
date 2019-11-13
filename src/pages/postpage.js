618bd3b974f5dee88323811f2718719c84b19d76
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-11-13 23:47:34 +0100

    feat: Implement pagination for blog posts

--- src/pages/postpage.js
+++ src/pages/postpage.js
@@ -5,8 +5,61 @@ export default class PostView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: []
-        }
+            posts: [],
+            currentPage: 1,
+            postsPerPage: 6,
+            upperPageBound: 6,
+            lowerPageBound: 0,
+            isPrevBtnActive: 'disabled',
+            isNextBtnActive: '',
+            pageBound: 6
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.btnNextClick = this.btnNextClick.bind(this);
+        this.btnPrevClick = this.btnPrevClick.bind(this);
+        this.setPrevAndNextBtnClass = this.setPrevAndNextBtnClass.bind(this);
+    }
+
+    handleClick(event) {
+      let listid = Number(event.target.id);
+      this.setState({
+        currentPage: listid
+      });
+      this.setPrevAndNextBtnClass(listid);
+    }
+    setPrevAndNextBtnClass(listid) {
+      let totalPage = Math.ceil(this.state.posts.length / this.state.postsPerPage);
+      this.setState({isNextBtnActive: 'disabled'});
+      this.setState({isPrevBtnActive: 'disabled'});
+      if(totalPage === listid && totalPage > 1){
+          this.setState({isPrevBtnActive: ''});
+      }
+      else if(listid === 1 && totalPage > 1){
+          this.setState({isNextBtnActive: ''});
+      }
+      else if(totalPage > 1){
+          this.setState({isNextBtnActive: ''});
+          this.setState({isPrevBtnActive: ''});
+      }
+    }
+
+    btnPrevClick() {
+      if((this.state.currentPage -1)%this.state.pageBound === 0 ){
+          this.setState({upperPageBound: this.state.upperPageBound - this.state.pageBound});
+          this.setState({lowerPageBound: this.state.lowerPageBound - this.state.pageBound});
+      }
+      let listid = this.state.currentPage - 1;
+      this.setState({ currentPage : listid});
+      this.setPrevAndNextBtnClass(listid);
+    }
+    btnNextClick() {
+      if((this.state.currentPage +1) > this.state.upperPageBound ){
+          this.setState({upperPageBound: this.state.upperPageBound + this.state.pageBound});
+          this.setState({lowerPageBound: this.state.lowerPageBound + this.state.pageBound});
+      }
+      let listid = this.state.currentPage + 1;
+      this.setState({ currentPage : listid});
+      this.setPrevAndNextBtnClass(listid);
     }
 
     componentDidMount () {
@@ -16,7 +69,33 @@ export default class PostView extends React.Component {
     }
 
     render() {
-        const { posts } = this.state;
+        const { posts, currentPage, postsPerPage, isPrevBtnActive, isNextBtnActive } = this.state;
+
+        // Logic for displaying current posts
+        const indexOfLastpost = currentPage * postsPerPage;
+        const indexOfFirstpost = indexOfLastpost - postsPerPage;
+        const currentposts = posts.slice(indexOfFirstpost, indexOfLastpost);
+
+        const renderposts = currentposts.map(post => <div key={post.id}>
+          <img alt="post" src={post.featured_image}/>
+          <Link className="link" to={`/${post.slug}`}><p>{post.title.rendered}</p></Link>
+          <p>{post.excerpt.rendered}</p>
+        </div>)
+
+        let renderPrevBtn = null;
+        if(isPrevBtnActive === 'disabled') {
+            renderPrevBtn = <div className={isPrevBtnActive}><span id="btnPrev"> Prev </span></div>
+        }
+        else{
+            renderPrevBtn = <div className={isPrevBtnActive}><a href='#' id="btnPrev" onClick={this.btnPrevClick}> Prev </a></div>
+        }
+        let renderNextBtn = null;
+        if(isNextBtnActive === 'disabled') {
+            renderNextBtn = <div className={isNextBtnActive}><span id="btnNext"> Next </span></div>
+        }
+        else{
+            renderNextBtn = <div className={isNextBtnActive}><a href='#' id="btnNext" onClick={this.btnNextClick}> Next </a></div>
+        }
 
         return (
             <div>
@@ -24,11 +103,11 @@ export default class PostView extends React.Component {
                 <h1>Epower Blog</h1>
               </div>
               <div className='posts'>
-              {posts.map(post => <div key={post.id}>
-                <img alt="post" src={post.featured_image}/>
-                <Link className="link" to={`/${post.slug}`}><p>{post.title.rendered}</p></Link>
-                <p>{post.excerpt.rendered}</p>
-              </div>)}
+              {renderposts}
+              </div>
+              <div className="pagination">
+              {renderPrevBtn}
+              {renderNextBtn}
               </div>
             </div>
         );
